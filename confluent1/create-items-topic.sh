c9fe7344c009d5c405ef1426a5ab63b52a2d4e64
diepet <diepet@gmail.com>
2018-09-21 16:32:37 +0200

    Added script to create a topic

---
+++ confluent1/create-items-topic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+export CONFLUENT_HOME=/opt/confluent-5.0.0
+$CONFLUENT_HOME/bin/kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic items
