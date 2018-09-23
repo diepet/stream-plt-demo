8d022ecb27005342561ddb5e7449b33800e34f69
Diego Pettisani <diepet@gmail.com>
2018-09-23 06:26:53 +0200

    Fixed receiver

--- receiver/src/main/java/it/diepet/streamplt/receiver/kafka/KafkaItemConsumer.java
+++ receiver/src/main/java/it/diepet/streamplt/receiver/kafka/KafkaItemConsumer.java
@@ -4,25 +4,26 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.vertx.core.eventbus.EventBus;
+
 public class KafkaItemConsumer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaItemConsumer.class);
-	private static KafkaItemConsumer instance = new KafkaItemConsumer();
 	private static final String TOPIC_NAME = "items";
 
 	private KafkaConsumer<String, String> consumer = null;
+	private EventBus eventBus;
+	
 
-	private KafkaItemConsumer() {
+	public KafkaItemConsumer(final EventBus eventBus) {
 		super();
-	}
-
-	public static final KafkaItemConsumer getInstance() {
-		return instance;
+		this.eventBus = eventBus;
 	}
 
 	private void init() {
@@ -42,6 +43,10 @@ public class KafkaItemConsumer {
 			if (!records.isEmpty()) {
 				LOGGER.debug("Received {} records to consume", records.count());
 				//KafkaItemProducer.getInstance().send(records);
+				for (final ConsumerRecord<String, String> record : records) {
+					LOGGER.debug("Received Kafka item ({}): {}", record.key(), record.value());
+					eventBus.send("items", record.value());
+				}
 				consumer.commitAsync();
 			}
 		}
--- receiver/src/main/java/it/diepet/streamplt/receiver/verticle/ReceiverVerticle.java
+++ receiver/src/main/java/it/diepet/streamplt/receiver/verticle/ReceiverVerticle.java
@@ -6,21 +6,41 @@ import org.slf4j.LoggerFactory;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServer;
+import io.vertx.ext.bridge.PermittedOptions;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import it.diepet.streamplt.receiver.kafka.KafkaItemConsumer;
 
 public class ReceiverVerticle extends AbstractVerticle {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReceiverVerticle.class);
-	
+
 	@Override
 	public void start(Future<Void> fut) {
 		HttpServer server = vertx.createHttpServer();
 		Router router = Router.router(vertx);
+		// static content configuration
 		router.route().handler(BodyHandler.create());
 		router.route("/static/*").handler(StaticHandler.create().setWebRoot("webcontent"));
+
+		// client event bus configuration
+		SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+		PermittedOptions outbound = new PermittedOptions().setAddress("items");
+		BridgeOptions options = new BridgeOptions().addOutboundPermitted(outbound);
+		sockJSHandler.bridge(options);
+
+		router.route("/eventbus/*").handler(sockJSHandler);
 		server.requestHandler(router::accept).listen(8081);
+		LOGGER.info("Receiver server listening on port 8081");
+
+		Runnable r = () -> {
+			KafkaItemConsumer consumer = new KafkaItemConsumer(vertx.eventBus());
+			consumer.start();
+		};
+		new Thread(r).start();
 	}
-	
+
 }
